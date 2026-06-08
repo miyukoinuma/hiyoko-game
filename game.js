296bb33b4e2445f51af7c405f2801ebcc795af0c
肥沼実結布 <miyu.koinuma@rakus-partners.co.jp>
2026-06-08 17:33:09 +0900

    ランキング修正

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const C = {W:480,H:720,BASE:30,SPD:280,MAX_LV:20,CHKN:12,
   FOODS:{rice:{e:'🍚',g:1,p:10,s:1,w:40},grain:{e:'🌾',g:2,p:25,s:1.2,w:30},
     peach:{e:'🍑',g:3,p:50,s:1.5,w:15}},
   ROTTEN:{e:'💀',g:0,p:0,s:1.3,deadly:true},
-  DL_PUB:'6a0ad2558f40bb17b0a37437',DL_PRI:'1AB4LThCe0Wdu7T9xQhM4AFwLJxWJih0Sle5Say_59MQ',DL_BASE:'https://api.codetabs.com/v1/proxy?quest=http://dreamlo.com/lb'
+  DL_PUB:'6a0ad2558f40bb17b0a37437',DL_PRI:'1AB4LThCe0Wdu7T9xQhM4AFwLJxWJih0Sle5Say_59MQ',DL_BASE:'https://corsproxy.io/?http://dreamlo.com/lb'
 };
 const growthNeeded=l=>{
   const baseNeed=2.8+l*1.25;
